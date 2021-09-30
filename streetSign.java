04d4084c556d40b4ce28bc2dc979f460d86bc412
cannonf <70234975+cannonf@users.noreply.github.com>
2021-09-30 10:03:31 -0500

    Add files via upload

---
+++ streetSign.java
@@ -0,0 +1,15 @@
+// Name: Cannon Flinders        Program: Create a picture
+// Description: This is simple program to create a small image
+// when the code is run. The image is a man holding a balloon
+public class streetSign {
+    public static void main(String[] args) {
+        System.out.println(" ..... (¯`v´¯)");
+        System.out.println(" .......•.¸.•´");
+        System.out.println(" ....¸.•´");
+        System.out.println(" ... )");
+        System.out.println("  ☻/");
+        System.out.println(" /▌♥♥");
+        System.out.println(" / / ♥♥");
+        // I use "ln" at the end of each print to make sure it is created in a new line
+    }
+}
